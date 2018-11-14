80e074a42d1bbd276986595be0e3a4e99033edf0
FuzzyFuze <45024155+FuzzyFuze@users.noreply.github.com>
2018-11-14 17:00:23 +1100

    Update BaseProduct.java

--- BaseProduct.java
+++ BaseProduct.java
@@ -1,9 +1,9 @@
 package assessment;
 
-public class BaseProduct extends Product {                                                          // this class is derived from the abstract Product class
+public class BaseProduct extends Product {                                                    // this class is derived from the abstract Product class
 	                                                                                            // it contains no additional attibutes or methods
 	public BaseProduct(String brand, String desc, double weight, double price, int quantity) {  // acting as a means to instantiate the abstract class
-		super(brand, desc, weight, price, quantity);                                        // for testing purposes
+		super(brand, desc, weight, price, quantity);                                              // for testing purposes
 	}
 
 	@Override
