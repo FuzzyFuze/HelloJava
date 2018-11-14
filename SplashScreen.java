846a91c6f7befb81b87263b5ee1fe55cef93a4fe
FuzzyFuze <45024155+FuzzyFuze@users.noreply.github.com>
2018-11-14 17:17:56 +1100

    Update SplashScreen.java

--- SplashScreen.java
+++ SplashScreen.java
@@ -1,46 +1,47 @@
 package assessment;
 
-import java.awt.*;																		// imports jdk abstract window tools module
-import javax.swing.*;																	// imports jdk GUI wisget module
+import java.awt.*;																		
+import javax.swing.*;																	
 
-public class SplashScreen extends JWindow {												// SplashScreen class inherits imported object class
+public class SplashScreen extends JWindow {												
 
-	private static final long serialVersionUID = 1L;									// auto-generated serialization attribute
-	private int duration;																// integer attribute to control screen timer
+	private static final long serialVersionUID = 1L;									
+	private int duration;																
 	
-	public SplashScreen(int duration) {													// constructor to initialize SplashScreen object with duration set
-		this.duration = duration;														// pickup parameter at instantiation time and assign to object
+	public SplashScreen(int duration) {													
+		this.duration = duration;														
 	}
-	public Color blueBorder = new Color(0, 0, 128, 255);								// instantiate Color object with RGBA attributes set
-	public Dimension screen = java.awt.Toolkit.getDefaultToolkit().getScreenSize();		// instantiate Dimension object as a default toolkit component
-	
-	public void drawSplash() {															// method to draw the SplashScreen content
-		int width = 225;																// declare integer to hold value for width
-		int height = 225;																// declare integer to hold value for height
-		int x = (screen.width - width) / 2;												// declare integer as screen width calculation for display width
-		int y = (screen.height - height) / 2;											// declare integer as screen height calculation for display height
-		setBounds(x, y, width, height);													// set the boundaries for the SplashScreen
-		JLabel image = new JLabel(new ImageIcon("loading.gif"));						// instantiate JLabel ImageIcon object with file source
-		JLabel title = new JLabel("STORE MANAGER DEMO", SwingConstants.CENTER);			// instantiate JLabel text object with String argument and center it
-		JPanel content = (JPanel) getContentPane();										// assign JPanel method call to define a ContentPane
-		content.setBackground(Color.white);												// set the background color of the ContentPane
-		content.add(image, BorderLayout.CENTER);										// add the instance of 'image' to the ContentPane and center it
-		content.add(title, BorderLayout.SOUTH);											// add the instance of 'title' to the ContentPane and align it 
-		content.setBorder(BorderFactory.createLineBorder(blueBorder, 4));				// set the border of the ContentPane and set the color to 'blueBorder'
-		title.setFont(new Font("Dialog", Font.BOLD, 16));								// set the font of 'title'
+  
+	public Color blueBorder = new Color(0, 0, 128, 255);								
+	public Dimension screen = java.awt.Toolkit.getDefaultToolkit().getScreenSize();		
+	public void drawSplash() {															
+		int width = 225;																
+		int height = 225;																
+		int x = (screen.width - width) / 2;												
+		int y = (screen.height - height) / 2;											
+		setBounds(x, y, width, height);													
+		JLabel image = new JLabel(new ImageIcon("loading.gif"));						
+		JLabel title = new JLabel("STORE MANAGER DEMO", SwingConstants.CENTER);			
+		JPanel content = (JPanel) getContentPane();										
+		content.setBackground(Color.white);												
+		content.add(image, BorderLayout.CENTER);										
+		content.add(title, BorderLayout.SOUTH);											
+		content.setBorder(BorderFactory.createLineBorder(blueBorder, 4));				
+		title.setFont(new Font("Dialog", Font.BOLD, 16));								
 	}
-	public void timeOut() {																// method to set the SplashScreen timer
-		setVisible(true);																// boolean state to make the SplashScreen visible
-		try {																			// try to execute next line
-			Thread.sleep(duration);														// sleep for 'duration' (as set at SplashScreen instantiation time)
+  
+	public void timeOut() {																
+		setVisible(true);																
+		try {																			
+			Thread.sleep(duration);														
 		} 
-		catch (Exception e) {															// catch Exceptions if the try fails
-			System.exit(0);																// exit from the runtime
+		catch (Exception e) {															
+			System.exit(0);																
 		}
-		setVisible(false);																// when try is sucessful, make the SplashScreen invisible
+		setVisible(false);																
 	}
-	public void showSplash() {															// method to call the previous methods
+	public void showSplash() {															
 		drawSplash();
 		timeOut();
 	}
-} // end of class
+}
