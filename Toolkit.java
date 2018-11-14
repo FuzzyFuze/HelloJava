a8d36567fe3bfb58b5f495d2c8dcf6c66b76808b
FuzzyFuze <45024155+FuzzyFuze@users.noreply.github.com>
2018-11-14 17:22:29 +1100

    Update Toolkit.java

--- Toolkit.java
+++ Toolkit.java
@@ -1,32 +1,32 @@
 package assessment;
 
-public class Toolkit extends Product {							// inherited class from Product {abstract} 
+public class Toolkit extends Product {							
 
-	private int pieces;											// declare integer to hold a value
+	private int pieces;											
 
-	public Toolkit() {											// default constructor
-		super();												// refernece to parent constructor
-		this.pieces = 0;										// handle parameters
+	public Toolkit() {											
+		super();												
+		this.pieces = 0;										
 	}
 
 	public Toolkit(String brand, String desc, double weight, 
-		double price, int quantity, int pieces) {				// full constructor
-		super(brand, desc, weight, price, quantity);			// refernece to parent constructor
-		this.pieces = pieces;									// handle parameters
+		double price, int quantity, int pieces) {				
+		super(brand, desc, weight, price, quantity);			
+		this.pieces = pieces;									
 	}
 
-	public void setPieces(int pieces) {							// set method for integer 'pieces'
-		this.pieces = pieces;									// handle parameters
+	public void setPieces(int pieces) {							
+		this.pieces = pieces;									
 	}
 
-	public int getPieces() {									// get method for integer 'pieces'
-		return pieces;											// return values
+	public int getPieces() {									
+		return pieces;											
 	}
 
-	@Override													// auto-generated
-	public String toString() {									// toString() method override
+	@Override													
+	public String toString() {									
 		return "Brand Name: " + brand + " \n" + "Description: " + desc + " \n" + "Item Weight: " + df1.format(weight)
 		+ "kg \n" + "Item Price: $" + df2.format(price) + " \n" + "Number Of Pieces: " + pieces + " \n"
 		+ "Quantity In Stock: " + quantity + " \n" + "------------------------------------------ \n\n";
 	}
-} // end of class
+}
