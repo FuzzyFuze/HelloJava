1f83df7e3355cab51909199d3bbfbcc8f342aab2
FuzzyFuze <45024155+FuzzyFuze@users.noreply.github.com>
2018-11-14 17:18:51 +1100

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,14 +1,14 @@
 package assessment;
 
-import java.io.FileNotFoundException;										// imports jdk '404' module (auto-generated)
+import java.io.FileNotFoundException;			
 
 public class Tester {
 
-    public static void main(String[] args) throws FileNotFoundException {	// main method - throws '404' (auto-generated)
-        SplashScreen splash = new SplashScreen(3300);						// instantiate SplashScreen object
-        UI ui = new UI();													// instantiate UI object
-        splash.showSplash();												// call showSplash() method
-        ui.initArray();														// call initArray() method
-        ui.startMenu();														// call startMenu() method
+    public static void main(String[] args) throws FileNotFoundException 
+        SplashScreen splash = new SplashScreen(3300);						
+        UI ui = new UI();													
+        splash.showSplash();												
+        ui.initArray();														
+        ui.startMenu();														
     }
-} // end of class
+} 
